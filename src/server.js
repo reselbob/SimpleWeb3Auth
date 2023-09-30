18d52bd539a21363e2b584584768d1391141f1dc
Bob Reselman <bob@cogarttech.com>
2023-09-29 23:19:01 -0700

    adding profile data add

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const ethUtil = require('ethereumjs-util');
 const sigUtil = require('eth-sig-util');
+const jwt = require('jsonwebtoken');
 const path = require('path'); // Required for file paths
 
 app.use(bodyParser.json());
@@ -12,32 +13,11 @@ const secretKey = 'w87LqcTUMeA7U8v@#yEEZX2KfH@G9mWxxx';
 
 // Mock user database (you should use a real database)
 const users = [];
-
-function extractNonce(inputString){
-    // Split the string into lines
-    const lines = inputString.split('\n');
-
-// Initialize a variable to store the Nonce value
-    let nonceValue = null;
-
-// Iterate through the lines to find the line containing "Nonce"
-    for (const line of lines) {
-        if (line.includes("Nonce:")) {
-            // Extract the Nonce value from the line
-            const parts = line.split("Nonce:");
-            if (parts.length === 2) {
-                nonceValue = parts[1].trim();
-                break; // Stop searching once the Nonce is found
-            }
-        }
-    }
-
-    return nonceValue;
-}
+const profiles= [];
 
 // Middleware to verify MetaMask signature
 function verifySignature(req, res, next) {
-    const { address, signature, message } = req.body;
+    const {address, signature, message} = req.body;
 
     const senderAddress = sigUtil.recoverPersonalSignature({
         data: message,
@@ -50,35 +30,86 @@ function verifySignature(req, res, next) {
             users.push(senderAddress.toLowerCase());
         }
 
-        // Signature is valida
+        // Signature is valid
         req.senderAddress = senderAddress;
         return next();
     } else {
-        return res.status(401).json({ error: 'Invalid signature' });
+        return res.status(401).json({error: 'Invalid signature'});
     }
 }
 
 // Route for handling MetaMask login
 app.post('/login', verifySignature, (req, res) => {
-    const { senderAddress } = req;
+    const {senderAddress} = req;
 
     // Check if the user is already registered
 
     if (users.includes(senderAddress.toLowerCase())) {
         // User exists, generate a JWT token (you should use a real authentication library)
         const token = generateAuthToken(senderAddress);
-        return res.status(200).json({ token });
+        return res.status(200).json({token});
         //return res.json({ token });
     } else {
         // User is not registered, you may choose to register them
-        return res.status(401).json({ error: 'User not registered' });
+        return res.status(401).json({error: 'User not registered'});
+    }
+});
+
+
+async function verifyJwtToken(req, res) {
+    const authorizationHeader = req.headers.authorization;
+
+    if (!authorizationHeader) return res.status(401).json({error: 'Authorization header missing'});
+
+    const tokenParts = authorizationHeader.split(' ');
+
+    if (tokenParts.length !== 2 || tokenParts[0] !== 'Bearer') {
+        return res.status(401).json({error: 'Invalid Authorization header format'});
+    }
+
+    const jwtToken = tokenParts[1];
+    let decoded;
+    try {
+        decoded = await jwt.verify(jwtToken, secretKey);
+    } catch (e) {
+        return res.status(401).json({error: e.message});
+    }
+        // Access decoded data (user information)
+    const userData = decoded;
+
+        // Process the data as needed
+    console.log('Decoded User Data:', userData);
+    return userData
+}
+// Route for handling MetaMask login
+app.post('/profile', async (req, res) => {
+    const tokenData = await verifyJwtToken(req, res)
+
+    // Check if the user is already registered
+
+    if (users.includes(tokenData.senderAddress.toLowerCase())) {
+        const {firstName, lastName, email} = req.body;
+        if (!profiles.some(obj => obj.address === tokenData.senderAddress)){
+            profiles.push({
+                address: tokenData.senderAddress,
+                firstName,
+                lastName,
+                email
+            })
+            return res.status(200).json("ok");
+        }else{
+            return res.status(201).json("ok");;
+        }
+    } else {
+        // User is not registered, you may choose to register them
+        return res.status(401).json({error: 'Invalid authentication'});
     }
 });
 
 // Helper function to generate a JWT token (replace with your own token generation logic)
 function generateAuthToken(senderAddress) {
-    // In a real application, you'd typically use a library like jsonwebtoken
-    return `Bearer ${senderAddress}`;
+    const token = jwt.sign({senderAddress}, secretKey, {expiresIn: '1h'});
+    return `Bearer ${token}`;
 }
 
 // Serve static files from the 'public' folder
